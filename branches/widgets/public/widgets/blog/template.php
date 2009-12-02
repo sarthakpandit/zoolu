23ca59bd6e8d3f9973cdadfabd2d6dca791e5438
flo.rian291 <flo.rian291@02016b66-6bd5-11de-bb38-9f61fe354615>
2009-12-02 14:49:28 +0000

    Pagination for Blog Widget

--- branches/widgets/public/widgets/blog/template.php
+++ branches/widgets/public/widgets/blog/template.php
@@ -4,7 +4,7 @@
       <div class="inner detail">        
         <!-- Sub Navigation -->        
         <!-- Main Content -->
-        <?php foreach($this->objEntries AS $entry): ?>
+        <?php foreach($this->paginator AS $entry): ?>
 	        <div class="divContentContainer">
 	        	<?php if (isset($entry->title)): print '<h1><a href="'.$this->strWidgetUrl.'/view/'.date("Y/m/d", strtotime($entry->created)).'/'. ereg_replace(' ','-',$entry->title).'">'. $entry->title .'</a></h1>'; endif; ?>
 	        	von <em><?php if (isset($entry->username)): print $entry->username; endif; ?></em> am <?php if (isset($entry->created)): print $entry->created; endif; ?><br/><br/>
@@ -13,6 +13,7 @@
 	        </div>
         <?php endforeach; ?>
         <div class="clear"></div>
+        <?php print $this->paginationControl($this->paginator, 'Sliding', 'templates/pagination.php'); ?>
       </div>  
     </div>
   </div>
