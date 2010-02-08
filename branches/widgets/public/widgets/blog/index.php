b451e046efd437e12f664eb2eeace7e1c0b78f56
flo.rian291 <flo.rian291@02016b66-6bd5-11de-bb38-9f61fe354615>
2010-02-08 17:16:32 +0000

    blog layout fixes

--- branches/widgets/public/widgets/blog/index.php
+++ branches/widgets/public/widgets/blog/index.php
@@ -11,14 +11,18 @@
         </div>     
         <!-- Main Content -->
         <div class="divContentContainer">
-        <?php foreach($this->objEntries AS $entry): ?>
-	        	<?php if (isset($entry->title)): print '<h1><a href="/'.strtolower($entry->languageCode) . '/'. $entry->url.'">'. $entry->title .'</a></h1>'; endif; ?>
-	        	von <em><?php if (isset($entry->username)): print $entry->username; endif; ?></em> am <?php if (isset($entry->created)): print $entry->created; endif; ?><br/><br/>
-	        	<?php if (isset($entry->text)): if(strlen($entry->text) > 1500): print substr($entry->text,0,1500).' ...'; else: print $entry->text; endif; endif; ?>
-	          <div class="clear"></div>
-        <?php endforeach; ?>
-        
-        <?php print $this->pager($this->total, $this->perPage)->paginate() ?>
+				<?php if($this->total > 0 ): ?>
+	        <?php foreach($this->objEntries AS $entry): ?>
+		        	<?php if (isset($entry->title)): print '<h1><a href="/'.strtolower($entry->languageCode) . '/'. $entry->url.'">'. $entry->title .'</a></h1>'; endif; ?>
+		        	von <em><?php if (isset($entry->username)): print $entry->username; endif; ?></em> am <?php if (isset($entry->created)): print $entry->created; endif; ?><br/><br/>
+		        	<?php if (isset($entry->text)): if(strlen($entry->text) > 1500): print substr($entry->text,0,1500).' ...'; else: print $entry->text; endif; endif; ?>
+		          <div class="clear"></div>
+	        <?php endforeach; ?>
+	        
+	        <?php print $this->pager($this->total, $this->perPage)->paginate() ?>
+	      <?php else: ?>
+	      	<p>Keine Eintr&auml;ge f&uuml;r diesen Blog vorhanden.</p>
+	      <?php endif; ?>
         </div>      
 	      <div class="clear"></div>
       </div>  
